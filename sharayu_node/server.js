55b0fe033f67affa7acda1f57fab7480d6e39796
ssDhavandkar <121050219+ssDhavandkar@users.noreply.github.com>
2023-01-02 14:57:13 +0530

    Update server.js

--- sharayu_node/server.js
+++ sharayu_node/server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 app.get("/",function(req,res){
-    res.sendFile("<h3>Name : Sharayu</h3><h3>RollNo : 229205</h3><h3>EmailId : sharayuD@gmail.com</h3><h3>Address : Pune</h3>");
+    res.send("<h3>Name : Sharayu</h3><h3>RollNo : 229205</h3><h3>EmailId : sharayuD@gmail.com</h3><h3>Address : Pune</h3>");
 });
 app.listen(4400);
 console.log("server is running at port 4400");
